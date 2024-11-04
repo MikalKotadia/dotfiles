43cd4667a832a3e5c2a9f017f7e1cc072e23d1c5
Mikal Kotadia <mikal.kotadia@truenorthmortgage.ca>
2024-11-04 08:40:08 -0700

    python formatting

--- .config/nvim/lua/configs/null-ls.lua
+++ .config/nvim/lua/configs/null-ls.lua
@@ -5,6 +5,7 @@ local b = null_ls.builtins
 local sources = {
     b.diagnostics.eslint,
     b.code_actions.eslint_d,
+    b.formatting.autopep8,
 }
 
 null_ls.setup {
--- .config/nvim/lua/configs/overrides.lua
+++ .config/nvim/lua/configs/overrides.lua
@@ -82,7 +82,7 @@ M.formatters = {
     formatters_by_ft = {
         lua = { "stylua" },
         -- Conform will run multiple formatters sequentially
-        python = { "isort", "black" },
+        python = { { "autopep8" } },
         -- Use a sub-list to run only the first available formatter
         javascript = { { "eslint_d" } },
         typescript = { { "eslint_d" } },
