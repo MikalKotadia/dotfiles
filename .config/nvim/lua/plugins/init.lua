0a34f69b4c4a9d1211d5ef844704503f163fdf12
Mikal Kotadia <mikal.kotadia@truenorthmortgage.ca>
2025-10-14 11:22:40 -0600

    md renderer

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -265,6 +265,14 @@ local plugins = {
       opts = {},
     },
   },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+    ft = { "markdown" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 }
 
 return plugins
