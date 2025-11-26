c63c9a49dd5dc62e652c6cb7ea4c681eaa203cc6
Mikal Kotadia <mikal.kotadia@truenorthmortgage.ca>
2025-11-26 10:16:16 -0700

    text obj support

--- .config/nvim/lua/configs/overrides.lua
+++ .config/nvim/lua/configs/overrides.lua
@@ -23,6 +23,79 @@ M.treesitter = {
     indent = {
         enable = true,
     },
+    textobjects = {
+        select = {
+            enable = true,
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+                ["aa"] = "@parameter.outer",
+                ["ia"] = "@parameter.inner",
+                ["ab"] = "@block.outer",
+                ["ib"] = "@block.inner",
+                ["al"] = "@loop.outer",
+                ["il"] = "@loop.inner",
+                ["ai"] = "@conditional.outer",
+                ["ii"] = "@conditional.inner",
+            },
+            -- You can choose the select mode (default is charwise 'v')
+            selection_modes = {
+                ['@parameter.outer'] = 'v', -- charwise
+                ['@function.outer'] = 'V', -- linewise
+                ['@class.outer'] = 'V', -- linewise
+            },
+            -- If you set this to `true` (default is `false`) then any textobject is
+            -- extended to include preceding or succeeding whitespace.
+            include_surrounding_whitespace = false,
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["]f"] = "@function.outer",
+                ["]c"] = "@class.outer",
+                ["]a"] = "@parameter.inner",
+            },
+            goto_next_end = {
+                ["]F"] = "@function.outer",
+                ["]C"] = "@class.outer",
+                ["]A"] = "@parameter.inner",
+            },
+            goto_previous_start = {
+                ["[f"] = "@function.outer",
+                ["[c"] = "@class.outer",
+                ["[a"] = "@parameter.inner",
+            },
+            goto_previous_end = {
+                ["[F"] = "@function.outer",
+                ["[C"] = "@class.outer",
+                ["[A"] = "@parameter.inner",
+            },
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ["<leader>sn"] = "@parameter.inner",
+            },
+            swap_previous = {
+                ["<leader>sp"] = "@parameter.inner",
+            },
+        },
+        lsp_interop = {
+            enable = true,
+            border = 'rounded',
+            floating_preview_opts = {},
+            peek_definition_code = {
+                ["<leader>pf"] = "@function.outer",
+                ["<leader>pc"] = "@class.outer",
+            },
+        },
+    },
 }
 
 M.lsp_servers = {
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -38,6 +38,9 @@ local plugins = {
   },
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     opts = overrides.treesitter,
   },
 
