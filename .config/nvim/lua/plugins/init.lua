a0159f8a1f237453fc4d92ca2c7a73c14b33b580
Mikal Kotadia <mikal.kotadia@truenorthmortgage.ca>
2026-04-01 13:58:58 -0600

    csv plugin setup

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -186,7 +186,7 @@ local plugins = {
     lazy = false,
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
-      require("nvim-ts-autotag").setup({
+      require("nvim-ts-autotag").setup {
         opts = {
           enable_close = true,
           enable_rename = true,
@@ -200,6 +200,42 @@ local plugins = {
             enable_close = true,
           },
         },
+      }
+    end,
+  },
+  {
+    "hat0uma/csvview.nvim",
+    ---@module "csvview"
+    ---@type CsvView.Options
+    opts = {
+      parser = { comments = { "#", "//" } },
+      keymaps = {
+        -- Text objects for selecting fields
+        textobject_field_inner = { "if", mode = { "o", "x" } },
+        textobject_field_outer = { "af", mode = { "o", "x" } },
+        -- Excel-like navigation:
+        -- Use <Tab> and <S-Tab> to move horizontally between fields.
+        -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
+        -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+        jump_next_row = { "<Enter>", mode = { "n", "v" } },
+        jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+      },
+    },
+    ft = { "csv" },
+    cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+    config = function(_, opts)
+      require("csvview").setup(opts)
+
+      local csvview_group = vim.api.nvim_create_augroup("csvview_auto_enable", { clear = true })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        group = csvview_group,
+        pattern = "csv",
+        callback = function()
+          vim.cmd("CsvViewEnable display_mode=border header_lnum=1")
+        end,
       })
     end,
   },
